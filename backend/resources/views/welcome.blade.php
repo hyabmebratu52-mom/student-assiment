37fe5494e1fb569b45015fc28af315351eed978f
amanawit <amanawitmebratu9@gmail.com>
2026-07-30 14:57:14 -0500

    Remove vite tags

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My React App</title>
-    @viteReactRefresh
-    @vite('resources/js/app.js')
 </head>
 <body>
     <div id="root"></div>
