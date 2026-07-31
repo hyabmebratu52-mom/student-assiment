2e6a3358917e63f6dd0384a16895fa7ef507cf76
amanawit <amanawitmebratu9@gmail.com>
2026-07-31 17:21:18 -0500

    Add CSP meta tag to upgrade insecure requests

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -1,12 +1,15 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My React App</title>
-    @viteReactRefresh
-    @vite('resources/js/app.js')
+    
+    <!-- 👈 አዲሱን መስመር እዚህ ጋር ጨምረው -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+    <title>Student Assignment</title>
 </head>
 <body>
-    <div id="root"></div>
+    <div id="app"></div>
 </body>
 </html>
