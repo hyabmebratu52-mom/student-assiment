04c95b02dd331570b91bdb7e261fe7929ff9c45f
amanawit <amanawitmebratu9@gmail.com>
2026-07-31 17:47:52 -0500

    Fix Vite script imports in welcome view

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!-- 👈 አዲሱን መስመር እዚህ ጋር ጨምረው -->
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
     <title>Student Assignment</title>
+    
+    <!-- React እና Vite በ Render ላይ በትክክል እንዲጫኑ የሚያስገድድ -->
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', 'resources/js/app.css'])
 </head>
 <body>
     <div id="app"></div>
